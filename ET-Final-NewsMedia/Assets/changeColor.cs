28a25583a3eac31b67c3992f14b6b57dd9321f15
Michael James <thelivingpc@gmail.com>
2016-11-20 17:06:19 -0500

    iteration 1
    bubbles and lights and colors

---
+++ ET-Final-NewsMedia/Assets/changeColor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeColor : MonoBehaviour {
+//	public Color color = new Color(0.2F, 0.3F, 0.4F, 0.5F);
+	public Color lerpedColor = Color.white;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		lerpedColor = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1));
+
+		Renderer rend = GetComponent<Renderer>();
+//		rend.material.shader = Shader.Find("Specular");
+		rend.material.SetColor("_Color", lerpedColor);
+	}
+}
